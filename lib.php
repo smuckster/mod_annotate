ebd4a876a44065ee47872deb77b23c6d1e2eaaee
Sam Smucker <sam.smucker@gmail.com>
2021-01-20 15:58:43 -0500

    Initial commit - the bare minimum to get a mod plugin to show up without errors in Moodle

---
+++ lib.php
@@ -0,0 +1,31 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>
+
+defined('MOODLE_INTERNAL') || die();
+
+/**
+ * Given an object containing all the necessary data,
+ * (defined by the form in mod_form.php) this function
+ * will create a new instance and return the id number
+ * of the new instance.
+ * 
+ * @param stdClass $annotate Add annotate instance
+ * @param mod_annotate_mod_form $mform
+ * @return int instance id
+ */
+function annotate_add_instance($annotate, $mform = null) {
+    global $CFG, $DB;
+}
